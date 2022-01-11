f4ac400e6f8ed370a2011f665bb8e2d90f739563
Your Name <you@example.com>
2022-01-12 00:17:22 +0100

    :bricks: split up in component

---
+++ paranormax/compenents/App/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./Header/Header";
+import Assignments from "./Content/Assigments/Assignments";
+import Footer from "./Footer/Footer";
+
+const App = () => {
+    return (
+        <>
+            <Header/>
+            <Assignments/>
+            <Footer/>
+        </>
+    );
+};
+
+export default App;
