90fdc3a9853239b8347e8b71b2c1f50d9bb1e5e6
Your Name <you@example.com>
2022-01-11 01:17:59 +0100

    :sparkles: YES succesfully made connection with the graphQL api with a bearer token

---
+++ paranormax/pages/index.js
@@ -0,0 +1,34 @@
+import {useQuery} from "@apollo/client";
+import GET_ASSIGNMENTS from "../lib/assigments/getAssigments";
+
+export default function Home() {
+
+  const AssQuery = useQuery(GET_ASSIGNMENTS);
+
+  const { data, loading, error } = AssQuery;
+
+  if (loading) {
+    return <h2>Loading...</h2>;
+  }
+
+  if (error) {
+    console.error(error);
+    return null;
+  }
+
+  if (data){
+    console.log(data)
+  }
+
+  const { entries } = data;
+
+  return (
+    <div>
+      {entries.map((entry)=>(
+          <div key={entry.id}>
+           <h2>{entry.title}</h2>
+          </div>
+      ))}
+    </div>
+  );
+}
